5d0c25d313a97e917ac83021baa1267de539973b
Octavio <ruarteoctavio8@gmail.com>
2023-07-29 14:54:37 -0300

    timeout copytext

--- client/src/components/RoomIdState.tsx
+++ client/src/components/RoomIdState.tsx
@@ -1,7 +1,7 @@
 import { ReactComponent as CopyIcon } from "../icons/copy-icon.svg"
 import { ReactComponent as CheckIcon } from "../icons/check-icon.svg"
 import { useCopyToClipboard } from 'usehooks-ts'
-import React from "react"
+import React, { useState } from "react"
 
 type Props = {
     roomId: string
@@ -9,22 +9,33 @@ type Props = {
 
 export const RoomIdState: React.FC<Props> = ({ roomId }) => {
     const [value, copy] = useCopyToClipboard()
-    const hasCopiedText = Boolean(value);
+    const [hasCopiedText, setHasCopiedText] = useState(false)
 
     return (
-        <article className="bg-slate-200 rounded-sm font-bold flex py-1 justify-around items-center px-4 gap-4">
+        <article className="bg-slate-200 rounded-sm font-bold flex py-1 justify-start items-center px-4 gap-4">
             <div className="h-2 w-2 "></div>
             <label>Room-ID:</label>
-            <div className="flex rounded-md border-2 ml-auto ">
+            <div className="flex rounded-md border-2 grow-1 ml-auto">
 
-                <p className="bg-slate-100 text-black rounded px-1 align-middle border border-slate-600">{roomId}</p>
-                <button
-                    disabled={hasCopiedText}
-                    className="link ml-2"
-                    onClick={() => copy(roomId)}
-                >
-                    {hasCopiedText ? <CheckIcon /> : <CopyIcon />}
-                </button>
+                <p className="bg-slate-100 text-black rounded pl-2 align-middle border border-slate-600 flex flex-row gap-1 justify-between grow-1">{roomId}
+
+                    <button
+                        disabled={hasCopiedText}
+                        className="link"
+                        onClick={() => {
+                            copy(roomId)
+                            setHasCopiedText(true)
+                            setTimeout(() => {
+                                setHasCopiedText(false)
+                            }, 3000)
+                        }}
+                    >
+                        <div className="bg-slate-500  rounded-e">
+
+                            {hasCopiedText ? <CheckIcon /> : <CopyIcon />}
+                        </div>
+                    </button>
+                </p>
             </div>
         </article>
     )
