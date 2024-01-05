650a38da6c4ccbf780d1657e64f32ce99bc8c7cf
Octavio Ruarte <ruarteoctavio8@gmail.com>
2024-01-05 20:58:39 -0300

    asd

--- client/src/components/ui/Layout.tsx
+++ client/src/components/ui/Layout.tsx
@@ -1,18 +1,18 @@
 import { Header } from "components/ui/Header";
 import { Footer } from "components/ui/Footer";
 import { Outlet } from "react-router-dom";
-import { useRecoilValue } from "recoil";
-import { currentGameStatus } from "atoms";
+import { useLocation } from "react-router-dom";
 
 function Layout() {
-	const gameStatus = useRecoilValue(currentGameStatus);
+	const location = useLocation();
+	console.log(location);
 	return (
 		<div className="h-screen bg-sky-900">
 			<Header />
 			<div className="container flex-grow p-10 w-full h-full m-auto">
 				<Outlet />
 			</div>
-			{gameStatus ? "" : <Footer />}
+			{location.pathname === "/game" ? "" : <Footer />}
 		</div>
 	);
 }
